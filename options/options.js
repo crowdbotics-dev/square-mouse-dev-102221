b9669262a8fb9b219ced4a63952ddc7a9e176d51
Crowdbotics <engineering@crowdbotics.com>
2023-07-17 11:42:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "square-mouse-dev-102221",
+  url: "https://square-mouse-dev-102221.botics.co",
+  api: "https://square-mouse-dev-102221.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
